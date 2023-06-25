6dce550c48fb0c74196a442f5cc6efa1950adf72
Sejung <49921709+sejungk@users.noreply.github.com>
2023-06-25 17:31:33 -0400

    individual item page created

--- component/MenuItemCard.jsx
+++ component/MenuItemCard.jsx
@@ -3,7 +3,9 @@ import styles from "../styles/MenuItemCard.module.css";
 const MenuItemCard = () => {
   return (
     <div className={styles.container}>
-      Pizza
+      <h5 className={styles.itemName}>Breakfast Egg Sandwich</h5>
+      <p className={styles.itemDesc}>Breakfast Egg Sandwich on your choice of bread</p>
+      <p className={styles.itemPrice}>$2.49</p>
     </div>
   )
 }
