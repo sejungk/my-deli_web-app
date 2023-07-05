a219b9b904e368a4ce0673698061c5766e31be2b
Sejung <49921709+sejungk@users.noreply.github.com>
2023-07-05 19:13:01 -0400

    created pickup details section

--- component/PickupDetails.jsx
+++ component/PickupDetails.jsx
@@ -7,17 +7,32 @@ const PickupDetails = () => {
       <div className={styles.pickupHeaderSection}>
         <h1>Pickup Details</h1>
       </div>
+
       <hr></hr>
-      <div className={styles.deliInfoSection}>
-        <div className={styles.storeLogo}>
-          <Image className={styles.logo} src="/img/store-icon.svg" layout="fill" alt="restaurant icon"/>
+
+      <div className={styles.pickupSection}>
+        <div className={styles.infoSection}>
+          <div className={styles.logoWrapper}>
+            <Image className={styles.logo} src="/img/store-icon.svg" layout="fill" alt="restaurant icon"/>
+          </div>
+          <div className={styles.infoWrapper}>
+            <h5>My Deli</h5>
+            <p>521 Broadway St, Quantico, VA 22134</p>
+            <a href="https://goo.gl/maps/39p3eL8vpdob73EC6">Get Directions</a>
+          </div>
         </div>
-        <div className={styles.storeInfo}>
-          <h5>My Deli</h5>
-          <p>521 Broadway St, Quantico, VA 22134</p>
-          <a href="https://goo.gl/maps/39p3eL8vpdob73EC6">Get Directions</a>
+        <hr></hr>
+
+        <div className={styles.infoSection}>
+          <div className={styles.logoWrapper}>
+            <Image className={styles.logo} src="/img/time-icon.svg" layout="fill" alt="time icon"/>
+          </div>
+          <div className={styles.infoWrapper}>
+            <p>Today at 6 pm</p>
+          </div>
         </div>
       </div>
+
     </div>
   )
 }
