1e69a2338f7bc0b0528ed934db51981358833976
Sejung <49921709+sejungk@users.noreply.github.com>
2023-07-10 19:59:16 -0400

    responsive design for order confirmation

--- app/orderConfirmation/[id]/page.jsx
+++ app/orderConfirmation/[id]/page.jsx
@@ -5,9 +5,12 @@ const OrderConfirmation = () => {
   return (
     <div className={styles.container}>
       <div className={styles.center}>
+        <div className={styles.logoMobile}>
+          <Image src="/img/green-check.svg" layout="fill" alt="location icon"/>
+        </div>
         <span class={styles.mainText}>Thank you for your order, John.</span>
         <div class={styles.descriptionText}>
-          <div class={styles.logo}>
+          <div class={styles.logoWeb}>
             <Image src="/img/green-check.svg" layout="fill" alt="location icon"/>
           </div>
           <h4>We’re preparing your pickup order for 3:30 am today. </h4>
