51b2ece7c8c4589464a96e73bd6a92f997584ed3
Sejung <49921709+sejungk@users.noreply.github.com>
2023-08-14 19:29:33 -0400

    refactor: Update database schema and queries

--- component/MenuItems.jsx
+++ component/MenuItems.jsx
@@ -7,11 +7,13 @@ import axios from "axios";
 
 const MenuItems = () => {
   const [menuItems, setMenuItems] = useState([]);
+
   useEffect(() => {
     // Fetch menu items from the backend API
     axios
       .get("http://localhost:5000/api/menu-items")
       .then((response) => {
+        console.log("API response:", response.data);
         setMenuItems(response.data);
       })
       .catch((error) => {
@@ -19,49 +21,58 @@ const MenuItems = () => {
       });
   }, []);
 
+  const groupedMenuItems = {}; // Object to store grouped menu items
+  console.log(groupedMenuItems)
+
+  // Group menu items by category
+  menuItems.forEach((menuItem) => {
+    const categoryName = menuItem.category_name;
+    if (!groupedMenuItems[categoryName]) {
+      groupedMenuItems[categoryName] = [];
+    }
+    groupedMenuItems[categoryName].push(menuItem);
+  });
+
+  // return (
+  //   <div className={styles.container}>
+  //     {Object.keys(groupedMenuItems).map((categoryName) => (
+  //       <div key={categoryName} className={styles.categoryText}>
+  //         <h1 className={styles.category}>{categoryName}</h1>
+  //         <div className={styles.wrapper}>
+  //           {groupedMenuItems[categoryName].map((menuItem) => (
+  //             <MenuItemCard
+  //               key={menuItem.id}
+  //               name={menuItem.name}
+  //               description={menuItem.description}
+  //               base_price={parseFloat(menuItem.base_price)}
+  //             />
+  //           ))}
+  //         </div>
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
+
   return (
     <div className={styles.container}>
-      <div className={styles.categoryText}>
-        <h1 className={styles.category}>Breakfast</h1>
-        <h3 className={styles.subcategory}>Breakfast Sandwiches</h3>
-      </div>
-      <div className={styles.wrapper}>
-        {menuItems.map((menuItem) => (
-          <MenuItemCard
-            key={menuItem.id}
-            name={menuItem.name}
-            description={menuItem.description}
-            base_price={parseFloat(menuItem.base_price)}
-          />
-        ))}
-      </div>
+      {Object.keys(groupedMenuItems).map((categoryName) => (
+        <div key={categoryName} className={styles.categoryText}>
+          <h1 className={styles.category}>{categoryName}</h1>
+          <div className={styles.wrapper}>
+            {groupedMenuItems[categoryName].map((menuItem) => (
+              <MenuItemCard
+                key={menuItem.menu_item_id}
+                name={menuItem.menu_item_name}
+                description={menuItem.menu_item_description}
+                base_price={parseFloat(menuItem.menu_item_base_price)}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default MenuItems;
 
-
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.categoryText}>
-//         <h1 className={styles.category}>Breakfast</h1>
-//         <h3 className={styles.subcategory}>Breakfast Sandwiches</h3>
-//       </div>
-//         <div className={styles.wrapper}>
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//           <MenuItemCard />
-//         </div>
-//     </div>
-//   )
-// }
