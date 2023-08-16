890268e2400cc233b47d869b5ac6991549c8b656
Sejung <49921709+sejungk@users.noreply.github.com>
2023-08-16 17:45:26 -0400

    Implemented menu item modal with dynamic data loading and options fetching
    - Added functionality to display menu item name and description
    - Updated MenuItemModal to fetch and display options data for the selected menu item
    - Implemented increase and decrease quantity functionality
    - Display total price based on quantity
    - Adjusted basic styling for the modal

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const pool = new Pool({
   port: 5432, // Default PostgreSQL port
 });
 
+// Route to get all menu items
 app.get("/api/menu-items", async (req, res) => {
   try {
     const query = `
@@ -31,14 +32,37 @@ app.get("/api/menu-items", async (req, res) => {
     JOIN categories ON menu_items.category_id = categories.category_id
     ORDER BY categories.category_id, menu_items.name;
     `;
-
-    // console.log("Query:", query); // Log the query
-
     const { rows } = await pool.query(query);
     res.json(rows);
   } catch (error) {
     console.error("Error fetching menu items:", error);
     res.status(500).json({ error: "An error occurred while fetching menu items." });
+  }
+});
+
+// Route to get a specific menu item by ID
+app.get("/api/menu-items/:id", async (req, res) => {
+  try {
+    const menuItemId = req.params.id; // Get the menu item ID from the URL parameter
+
+    const query = `
+      SELECT menu_items.id AS menu_item_id,
+             menu_items.name AS menu_item_name,
+             menu_items.description AS menu_item_description,
+             menu_items.base_price AS menu_item_base_price,
+             menu_items.exclusions AS menu_item_exclusions,
+             categories.category_id,
+             categories.name AS category_name
+      FROM menu_items
+      JOIN categories ON menu_items.category_id = categories.category_id
+      WHERE menu_items.id = $1;
+    `;
+
+    const { rows } = await pool.query(query, [menuItemId]);
+    res.json(rows);
+  } catch (error) {
+    console.error("Error fetching menu item:", error);
+    res.status(500).json({ error: "An error occurred while fetching the menu item." });
   }
 });
 
