9fe07b97448789c251f5a005706e4af8d5199220
Sejung <49921709+sejungk@users.noreply.github.com>
2023-08-17 20:33:19 -0400

    feat: Create CartItem component for better organization in Cart module
    - Created a new CartItem component to handle individual items within the cart for improved code organization.
    - Utilized dummy data to test and demonstrate the functionality of the CartItem component.
    - Updated the Cart component to use the CartItem component for rendering each item in the cart.
    - Ensured a clear separation of concerns between the Cart and CartItem components for easier maintenance and scalability.

--- component/Cart.jsx
+++ component/Cart.jsx
@@ -1,100 +1,65 @@
+import React from 'react';
 import styles from "../styles/Cart.module.css";
 import Image from "next/image";
+import CartItem from "./CartItem";
 
-const Cart = ( ) => {
+const Cart = () => {
+  const dummyCartItems = [
+    {
+      quantity: 1,
+      name: "Egg and Cheese Sandwich",
+      price: 2.99,
+      addOns: ["Wheat bread", "American Cheese", "Provolone Cheese"],
+    },
+    {
+      quantity: 2,
+      name: "Ham and Swiss Sandwich",
+      price: 3.49,
+      addOns: ["Rye bread", "Swiss Cheese", "Lettuce"],
+    },
+    // Add more dummy cart items here...
+  ];
+
   return (
     <div className={styles.container}>
-        <div className={styles.pickupDetailSection}>
-          <h5>Pickup Order Summary</h5>
-          <div className={styles.logoContainer}>
-            <div className={styles.logo}>
-              <Image src="/img/location-icon.svg" layout="fill" alt="location icon"/>
-            </div>
-            <p>521 Broadway St, Quantico</p>
+      <div className={styles.pickupDetailSection}>
+        <h5>Pickup Order Summary</h5>
+        <div className={styles.logoContainer}>
+          <div className={styles.logo}>
+            <Image src="/img/location-icon.svg" layout="fill" alt="location icon" />
           </div>
-          <div className="bttn bttn_outline bttn_center">
-              <span>Mon, Jun 12 8:45 am</span>
-          </div>
+          <p>521 Broadway St, Quantico</p>
         </div>
-
-        <hr></hr>
-
-        <div className={styles.addedItemSection}>
-          {/* item #1 - start */}
-          <div className={styles.singleItemContainer}>
-            <div className={styles.singleItemHeader}>
-              <span className={styles.itemQuantity}>1</span>
-              <span className={styles.itemName}>Egg and Cheese Sandwich</span>
-              <span className={styles.itemPrice}>$2.99</span>
-            </div>
-            <ol className={styles.addOnsList}>
-              <li><p>Wheat bread</p></li>
-              <li><p>American Cheese</p></li>
-              <li><p>Provolone Cheese</p></li>
-            </ol>
-            <div className={styles.editRemoveOptions}>
-              <a href="url">Edit</a>
-              <a href="url">Remove</a>
-            </div>
-          </div>
-          {/* item #1 - end */}
-
-          <hr></hr>
-
-          {/* item #2 - start */}
-          <div className={styles.singleItemContainer}>
-            <div className={styles.singleItemHeader}>
-              <span className={styles.itemQuantity}>1</span>
-              <span className={styles.itemName}>Egg and Cheese Sandwich</span>
-              <span className={styles.itemPrice}>$2.99</span>
-            </div>
-            <ol className={styles.addOnsList}>
-              <li><p>Wheat bread</p></li>
-              <li><p>American Cheese</p></li>
-              <li><p>Provolone Cheese</p></li>
-            </ol>
-            <div className={styles.editRemoveOptions}>
-              <a href="url">Edit</a>
-              <a href="url">Remove</a>
-            </div>
-          </div>
-          {/* item #2 - end */}
-
-          <hr></hr>
-
-          {/* item #3 - start */}
-          <div className={styles.singleItemContainer}>
-            <div className={styles.singleItemHeader}>
-              <span className={styles.itemQuantity}>1</span>
-              <span className={styles.itemName}>Egg and Cheese Sandwich</span>
-              <span className={styles.itemPrice}>$2.99</span>
-            </div>
-            <ol className={styles.addOnsList}>
-              <li><p>Wheat bread</p></li>
-              <li><p>American Cheese</p></li>
-              <li><p>Provolone Cheese</p></li>
-            </ol>
-            <div className={styles.editRemoveOptions}>
-              <a href="url">Edit</a>
-              <a href="url">Remove</a>
-            </div>
-          </div>
-          {/* item #3 - end */}
+        <div className="bttn bttn_outline bttn_center">
+          <span>Mon, Jun 12 8:45 am</span>
         </div>
+      </div>
 
-        <hr></hr>
+      <hr />
 
-        <div className={styles.subtotalSection}>
-          <div className={styles.subtotalInfo}>
-            <h5>Subtotal</h5>
-            <h5>$2.99</h5>
-          </div>
-          <div className="bttn bttn_red bttn_center">
-              <span>Checkout</span>
-          </div>
+      {/* middle cart items section */}
+      <div className={styles.addedItemSection}>
+        {dummyCartItems.map((item, index) => (
+          <React.Fragment key={index}>
+            <CartItem item={item} />
+            {index < dummyCartItems.length - 1 && <hr />} {/* Add <hr> except for the last item */}
+          </React.Fragment>
+        ))}
+      </div>
+
+      <hr />
+
+      <div className={styles.subtotalSection}>
+        <div className={styles.subtotalInfo}>
+          <h5>Subtotal</h5>
+          <h5>$2.99</h5>
         </div>
+        <div className="bttn bttn_red bttn_center">
+          <span>Checkout</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
---
+++ component/CartItem.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "../styles/CartItem.module.css";
+
+const CartItem = ({ item }) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.singleItemHeader}>
+        <span className={styles.itemQuantity}>{item.quantity}</span>
+        <span className={styles.itemName}>{item.name}</span>
+        <span className={styles.itemPrice}>${item.price.toFixed(2)}</span>
+      </div>
+      <ol className={styles.addOnsList}>
+        {item.addOns.map((addOn, index) => (
+          <li key={index}>
+            <p>{addOn}</p>
+          </li>
+        ))}
+      </ol>
+      <div className={styles.editRemoveOptions}>
+        <a href="url">Edit</a>
+        <a href="url">Remove</a>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
