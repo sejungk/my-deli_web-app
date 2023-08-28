3af2bd2fb9dd025db540b2b688a910f4fc8ac669
Sejung <49921709+sejungk@users.noreply.github.com>
2023-08-27 21:37:03 -0400

    feat: Implemented route for checkout
    - Added a new route '/checkout' for the checkout page.
    - Updated CartContext.js to correctly use the useRouter hook for navigation.
    - Now able to navigate to /checkout when the 'Checkout' button is pressed in the Cart component.

---
+++ component/Checkout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Checkout = () => {
+  return (
+    <div>
+      {/* Your checkout page content goes here */}
+      <h1>Checkout Page</h1>
+    </div>
+  );
+};
+
+export default Checkout;
