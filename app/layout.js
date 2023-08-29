2c975098d3e2fdb7a7eddaef1f05c0a7306a2708
Sejung <49921709+sejungk@users.noreply.github.com>
2023-08-29 16:40:53 -0400

    feat: Separate Layout into RootLayout and Layout
    - Created RootLayout, a top-level layout component for the entire application.
    - Moved the Navbar and Footer components into Layout.
    - Added metadata and integrated the Inter font in RootLayout.
    - Nested the existing Layout component inside RootLayout to maintain the desired structure.
    - Updated the imports and references to reflect the new layout structure.

--- app/layout.js
+++ app/layout.js
@@ -1,45 +1,16 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-  title: 'My Deli',
-  description: 'My Deli restaurant located in Quantico, Virginia',
-}
+import Footer from '../component/Footer';
+import Navbar from '../component/Navbar';
+import RootLayout from './RootLayout';
 
 const Layout = ({ children }) => {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
+    <RootLayout>
+      <Navbar />
+      {children}
+      <Footer />
+    </RootLayout>
   );
 }
 
 export default Layout;
 
-// import './globals.css'
-// import { Inter } from 'next/font/google'
-// import Footer from '../component/Footer';
-// import Navbar from '../component/Navbar';
-
-// const inter = Inter({ subsets: ['latin'] })
-
-// export const metadata = {
-//   title: 'My Deli',
-//   description: 'My Deli restaurant located in Quantico, Virginia',
-// }
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en">
-//       <body className={inter.className}>
-//         <Navbar />
-//         {children}
-//         <Footer />
-//       </body>
-//     </html>
-//   )
-// }
