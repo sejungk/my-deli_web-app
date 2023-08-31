134df5d0adc1dec76095a3198f5bffdb23b4deaf
Sejung <49921709+sejungk@users.noreply.github.com>
2023-08-31 15:17:05 -0400

    feat: Display Cart Items in OrderSummary Checkout Page
    - Implemented functionality to display cart items in the OrderSummary component on the checkout page.
    - Cart items are retrieved and rendered based on the provided data.

--- app/page.js
+++ app/page.js
@@ -1,16 +1,13 @@
-// 'use client'
-
 import React from 'react';
-import { CartProvider } from './CartContext';
 import MenuItems from '../component/MenuItems';
 import Cart from '../component/Cart';
 
 const Home = () => {
   return (
-    <CartProvider>
+    <>
       <MenuItems/>
       <Cart/>
-    </CartProvider>
+    </>
   );
 }
 
