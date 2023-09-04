e94cb5a0b1e8f8f65a1493f79b045b4c918e303a
Sejung <49921709+sejungk@users.noreply.github.com>
2023-09-04 15:58:15 -0400

    fix: Address issues in PickupDateModal component
    - Corrected date options to show the correct tomorrow's date
    - Adjusted time options to display the full range when applicable
    - Ensured the earliest pickup time is at least 15 minutes in the future

--- component/PickupDateModal.jsx
+++ component/PickupDateModal.jsx
@@ -6,65 +6,92 @@ import Image from "next/image";
 const PickupDateModal = ({ onCancel }) => {
   const [selectedDate, setSelectedDate] = useState('');
   const [selectedTime, setSelectedTime] = useState('');
-
-  // Function to generate an array of dates for the next 2 weeks
-  const generateDateOptions = () => {
-    const options = [];
-    const today = new Date();
-    const currentTime = new Date();
-
-    if (currentTime.getHours() >= 15) {
-      // If it's past 3 PM, show tomorrow's date
-      today.setDate(today.getDate() + 1);
-    }
-
-    for (let i = 0; i < 14; i++) {
-      const date = new Date(today);
-      date.setDate(today.getDate() + i);
-      const dateString = date.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' });
-      options.push(dateString);
-    }
-    return options;
-  };
-
-  // Function to generate an array of time options in 15-minute intervals
-  const generateTimeOptions = () => {
-    const options = [];
-    const currentTime = new Date();
-    const startTime = new Date(currentTime);
-    startTime.setHours(6, 0, 0);
-
-    const endTime = new Date(currentTime);
-    endTime.setHours(15, 0, 0);
-
-    if (currentTime.getHours() >= 15) {
-      // If it's past 3 PM, set the start time to 6 AM for the next day
-      startTime.setDate(startTime.getDate() + 1);
-      startTime.setHours(6, 0, 0);
-    }
-
-    const interval = 15 * 60 * 1000; // 15 minutes in milliseconds
-
-    let nextAvailableTime = new Date(Math.ceil(currentTime.getTime() / interval) * interval); // Round up to the nearest 15 minutes
-
-    while (nextAvailableTime <= endTime) {
-      const hours = nextAvailableTime.getHours();
-      const minutes = nextAvailableTime.getMinutes();
-      const timeString = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${hours >= 12 ? 'PM' : 'AM'}`;
-      options.push(timeString);
-      nextAvailableTime = new Date(nextAvailableTime.getTime() + interval);
-    }
-    return options;
-  };
-
-  const dateOptions = generateDateOptions();
-  const timeOptions = generateTimeOptions();
+  const [dateOptions, setDateOptions] = useState([]);
+  const [timeOptions, setTimeOptions] = useState([]);
+  const time230 = new Date(0, 0, 0, 2, 30, 0, 0);
 
   useEffect(() => {
+    // Function to generate an array of dates for the next 2 weeks
+    const generateDateOptions = () => {
+      const options = [];
+      const today = new Date();
+      const currentTime = new Date();
+
+      if (currentTime.getTime() > time230.getTime()) {
+        today.setDate(today.getDate() + 1);
+      }
+
+      for (let i = 0; i < 14; i++) {
+        const date = new Date(today);
+        date.setDate(today.getDate() + i);
+        const dateString = date.toLocaleDateString('en-US', {
+          weekday: 'short',
+          month: 'short',
+          day: 'numeric',
+        });
+        options.push(dateString);
+      }
+      return options;
+    };
+
+    // Function to generate an array of time options in 15-minute intervals
+    const generateTimeOptions = () => {
+      const options = [];
+      const currentTime = new Date();
+      const startTime = new Date(currentTime);
+      startTime.setHours(6, 0, 0);
+
+      const endTime = new Date(currentTime);
+      endTime.setHours(14, 30, 0); // Restaurant open hours until 2:30 PM
+
+      const interval = 15 * 60 * 1000; // 15 minutes in milliseconds
+
+      // Start from the greater of startTime and currentTime
+      let nextAvailableTime = new Date(Math.max(startTime, currentTime));
+      if (currentTime.getTime() > time230.getTime()) nextAvailableTime = startTime;
+
+      while (nextAvailableTime <= endTime) {
+        const hours = nextAvailableTime.getHours();
+        const minutes = nextAvailableTime.getMinutes();
+
+        if (currentTime.getTime() > time230.getTime() || timeDifference >= 15) {
+          const timeString = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${
+            hours >= 12 ? 'PM' : 'AM'
+          }`;
+          options.push(timeString);
+        }
+
+        nextAvailableTime = new Date(nextAvailableTime.getTime() + interval);
+      }
+      //   if (currentTime.getTime() > time230.getTime()) {
+      //     const timeString = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${
+      //       hours >= 12 ? 'PM' : 'AM'
+      //     }`;
+      //     options.push(timeString);
+      //     nextAvailableTime = new Date(nextAvailableTime.getTime() + interval);
+      //   }
+      //   else if (timeDifference >= 15) {
+      //     const timeString = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${
+      //       hours >= 12 ? 'PM' : 'AM'
+      //     }`;
+      //     options.push(timeString);
+      //     nextAvailableTime = new Date(nextAvailableTime.getTime() + interval);
+      //   }
+      // }
+
+      return options;
+    };
+
+    const dateOpts = generateDateOptions();
+    const timeOpts = generateTimeOptions();
+
+    setDateOptions(dateOpts);
+    setTimeOptions(timeOpts);
+
     // Set initial values for selectedDate and selectedTime
-    setSelectedDate(dateOptions[0]);
-    setSelectedTime(timeOptions[0]);
-  }, [dateOptions, timeOptions]);
+    setSelectedDate(dateOpts[0]);
+    setSelectedTime(timeOpts[0]);
+  }, []);
 
   return ReactDOM.createPortal(
     <div className={styles.container}>
@@ -83,7 +110,11 @@ const PickupDateModal = ({ onCancel }) => {
             <div className={styles.contentContainer}>
               <div className={styles.selectContainer}>
                 <div className={styles.selectWrapper}>
-                  <select className={styles.selectBox} value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)}>
+                  <select
+                    className={styles.selectBox}
+                    value={selectedDate}
+                    onChange={(e) => setSelectedDate(e.target.value)}
+                  >
                     {dateOptions.map((date, index) => (
                       <option key={index} value={date}>
                         {date}
@@ -93,7 +124,11 @@ const PickupDateModal = ({ onCancel }) => {
                 </div>
 
                 <div className={styles.selectWrapper}>
-                  <select className={styles.selectBox} value={selectedTime} onChange={(e) => setSelectedTime(e.target.value)}>
+                  <select
+                    className={styles.selectBox}
+                    value={selectedTime}
+                    onChange={(e) => setSelectedTime(e.target.value)}
+                  >
                   {timeOptions.map((time, index) => (
                       <option key={index} value={time}>
                         {time}
@@ -101,7 +136,6 @@ const PickupDateModal = ({ onCancel }) => {
                     ))}
                   </select>
                 </div>
-
               </div>
 
               <div className={styles.buttonWrapper}>
