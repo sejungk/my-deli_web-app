7b5522cf5710aee7f01dbe8feab63269643aa017
Sejung <49921709+sejungk@users.noreply.github.com>
2023-09-05 17:50:09 -0400

    feat: Implement PickupDateModal and use date and time options from parent
    - Created the  component to handle pickup date and time selection.
    - Passed  and  as props to  from the parent component.
    - Modified  to use the provided date and time options for initial values.

--- component/Cart.jsx
+++ component/Cart.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { createContext, useContext, useState} from 'react';
+import React, { useEffect, useContext, useState} from 'react';
 import styles from "../styles/Cart.module.css";
 import { CartContext } from '../app/CartContext';
 import Image from "next/image";
@@ -11,10 +11,92 @@ import MenuItemModal from './MenuItemModal';
 
 const Cart = () => {
   const [isPickupDateModalVisible, setIsPickupDateModalVisible] = useState(false);
+  const [selectedPickupDateTime, setSelectedPickupDateTime] = useState(null);
+  const [dateOptions, setDateOptions] = useState([]);
+  const [timeOptions, setTimeOptions] = useState([]);
+  const time230 = new Date(0, 0, 0, 2, 30, 0, 0);
+
+  console.log(selectedPickupDateTime);
+  // setSelectedPickupDateTime({ date, time });
   const { cartItems, checkout, editItemData, setEditItem, removeFromCart } = useContext(CartContext);
+
+  useEffect(() => {
+    // Function to generate an array of dates for the next 2 weeks
+    const generateDateOptions = () => {
+      const options = [];
+      const today = new Date();
+      const currentTime = new Date();
+
+      if (currentTime.getTime() > time230.getTime()) {
+        today.setDate(today.getDate() + 1);
+      }
+
+      for (let i = 0; i < 14; i++) {
+        const date = new Date(today);
+        date.setDate(today.getDate() + i);
+        const dateString = date.toLocaleDateString('en-US', {
+          weekday: 'short',
+          month: 'short',
+          day: 'numeric',
+        });
+        options.push(dateString);
+      }
+      return options;
+    };
+
+    // Function to generate an array of time options in 15-minute intervals
+    const generateTimeOptions = () => {
+      const options = [];
+      const currentTime = new Date();
+      const startTime = new Date(currentTime);
+      startTime.setHours(6, 0, 0);
+
+      const endTime = new Date(currentTime);
+      endTime.setHours(14, 30, 0); // Restaurant open hours until 2:30 PM
+
+      const interval = 15 * 60 * 1000; // 15 minutes in milliseconds
+
+      // Start from the greater of startTime and currentTime
+      let nextAvailableTime = new Date(Math.max(startTime, currentTime));
+      if (currentTime.getTime() > time230.getTime()) nextAvailableTime = startTime;
+
+      while (nextAvailableTime <= endTime) {
+        const hours = nextAvailableTime.getHours();
+        const minutes = nextAvailableTime.getMinutes();
+
+        if (currentTime.getTime() > time230.getTime() || timeDifference >= 15) {
+          const timeString = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${
+            hours >= 12 ? 'PM' : 'AM'
+          }`;
+          options.push(timeString);
+        }
+
+        nextAvailableTime = new Date(nextAvailableTime.getTime() + interval);
+      }
+
+      return options;
+    };
+    const dateOpts = generateDateOptions();
+    const timeOpts = generateTimeOptions();
+
+    setDateOptions(dateOpts);
+    setTimeOptions(timeOpts);
+
+    // Set initial values for selectedDate and selectedTime
+    setSelectedPickupDateTime({ date: dateOpts[0], time: timeOpts[0] });
+  }, []);
 
   const handleCheckout = () => {
     checkout();
+  };
+
+  // Callback function to handle the selected date and time
+  const handlePickupDateTimeSelection = (date, time) => {
+    // Only update the state if a different date or time is selected
+    if (date !== selectedPickupDateTime.date || time !== selectedPickupDateTime.time) {
+      setSelectedPickupDateTime({ date, time });
+      // You can also perform any other actions you need with the selected date and time here
+    }
   };
 
   const handleTogglePickupDateModal = () => {
@@ -66,11 +148,18 @@ const Cart = () => {
           <p>521 Broadway St, Quantico</p>
         </div>
         <div className="bttn bttn_outline bttn_center" onClick={handleTogglePickupDateModal}>
-          <span>Mon, Jun 12 8:45 am</span>
+          {selectedPickupDateTime ? (
+            <span>{`${selectedPickupDateTime.date} ${selectedPickupDateTime.time}`}</span>
+          ) : (
+            <span>Select Pickup Date &amp; Time</span>
+          )}
         </div>
         {isPickupDateModalVisible && (
           <PickupDateModal
             onCancel={handleTogglePickupDateModal}
+            onSelectDateTime={handlePickupDateTimeSelection}
+            dateOptions={dateOptions}
+            timeOptions={timeOptions}
           />
         )}
       </div>
@@ -105,14 +194,6 @@ const Cart = () => {
           </div>
         </Link>
       </div>
-      {/* {isMenuItemModalVisible && (
-        <MenuItemModal
-          menuItem={editItemData}
-          closeModal={closeModal}
-          editItemData={editItemData}
-          onSaveEdit={handleSaveEdit}
-        />
-      )} */}
     </div>
   );
 };
--- component/PickupDateModal.jsx
+++ component/PickupDateModal.jsx
@@ -3,80 +3,15 @@ import ReactDOM from 'react-dom';
 import styles from "../styles/ItemModal.module.css";
 import Image from "next/image";
 
-const PickupDateModal = ({ onCancel }) => {
+const PickupDateModal = ({ onCancel, onSelectDateTime, dateOptions, timeOptions }) => {
   const [selectedDate, setSelectedDate] = useState('');
   const [selectedTime, setSelectedTime] = useState('');
-  const [dateOptions, setDateOptions] = useState([]);
-  const [timeOptions, setTimeOptions] = useState([]);
-  const time230 = new Date(0, 0, 0, 2, 30, 0, 0);
 
-  useEffect(() => {
-    // Function to generate an array of dates for the next 2 weeks
-    const generateDateOptions = () => {
-      const options = [];
-      const today = new Date();
-      const currentTime = new Date();
-
-      if (currentTime.getTime() > time230.getTime()) {
-        today.setDate(today.getDate() + 1);
-      }
-
-      for (let i = 0; i < 14; i++) {
-        const date = new Date(today);
-        date.setDate(today.getDate() + i);
-        const dateString = date.toLocaleDateString('en-US', {
-          weekday: 'short',
-          month: 'short',
-          day: 'numeric',
-        });
-        options.push(dateString);
-      }
-      return options;
-    };
-
-    // Function to generate an array of time options in 15-minute intervals
-    const generateTimeOptions = () => {
-      const options = [];
-      const currentTime = new Date();
-      const startTime = new Date(currentTime);
-      startTime.setHours(6, 0, 0);
-
-      const endTime = new Date(currentTime);
-      endTime.setHours(14, 30, 0); // Restaurant open hours until 2:30 PM
-
-      const interval = 15 * 60 * 1000; // 15 minutes in milliseconds
-
-      // Start from the greater of startTime and currentTime
-      let nextAvailableTime = new Date(Math.max(startTime, currentTime));
-      if (currentTime.getTime() > time230.getTime()) nextAvailableTime = startTime;
-
-      while (nextAvailableTime <= endTime) {
-        const hours = nextAvailableTime.getHours();
-        const minutes = nextAvailableTime.getMinutes();
-
-        if (currentTime.getTime() > time230.getTime() || timeDifference >= 15) {
-          const timeString = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${
-            hours >= 12 ? 'PM' : 'AM'
-          }`;
-          options.push(timeString);
-        }
-
-        nextAvailableTime = new Date(nextAvailableTime.getTime() + interval);
-      }
-
-      return options;
-    };
-
-    const dateOpts = generateDateOptions();
-    const timeOpts = generateTimeOptions();
-
-    setDateOptions(dateOpts);
-    setTimeOptions(timeOpts);
-
-    // Set initial values for selectedDate and selectedTime
-    setSelectedDate(dateOpts[0]);
-    setSelectedTime(timeOpts[0]);
-  }, []);
+  const handleDateTimeSelection = () => {
+    // Call the callback function with the selected date and time
+    onSelectDateTime(selectedDate, selectedTime);
+    onCancel(); // Close the modal or perform other actions
+  };
 
   return ReactDOM.createPortal(
     <div className={styles.container}>
@@ -125,7 +60,7 @@ const PickupDateModal = ({ onCancel }) => {
 
               <div className={styles.buttonWrapper}>
                 <button className="bttn bttn_red bttn_center">
-                  <span>Update</span>
+                  <span onClick={handleDateTimeSelection}>Update</span>
                 </button>
               </div>
             </div>
