24e23a9eb85ac8ee03ad254015e65cf4a2b39cec
Sejung <49921709+sejungk@users.noreply.github.com>
2023-09-14 21:15:29 -0400

    style: Updated Prechecked Checkbox/Radio Input for Nested Selected Options
    - Refactored the checkbox/radio input handling in the ModalOptionGroup component.
    - Accommodated the updated selectedOption structure, which now includes nested objects.
    - Ensured that previously selected options are correctly checked when the modal is opened for editing.
    - Modified input handling logic to traverse nested selected options.
    - Checked if selectedOption[optionGroup.name][option.id] exists to determine selection.

--- component/ModalOptionGroup.jsx
+++ component/ModalOptionGroup.jsx
@@ -15,14 +15,15 @@ const ModalOptionGroup = ({ optionGroup, selectedOption, handleOptionChange, ope
         {optionGroup.options.map((option, index) => (
           <div className={styles.listOption} key={option.id}>
             <label className={styles.label}>
+            {console.log(selectedOption[option.id], option.id)}
               <input
                 className={styles.option}
                 type={optionGroup.allow_multiple ? 'checkbox' : 'radio'}
                 value={option.name}
                 name={optionGroup.name}
                 {...(operationType === "edit" &&
-                selectedOption &&
-                 selectedOption.id === option.id && { checked: true })}
+                selectedOption[option.id] &&
+                selectedOption[option.id].id === option.id && { checked: true })}
                 onChange={() => {
                   handleOptionChange(optionGroup, option);
                 }}
