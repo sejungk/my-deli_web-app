da5f67fae93cad296b9e6f10956a1f87ffe9ff99
Sejung <49921709+sejungk@users.noreply.github.com>
2023-09-17 19:03:12 -0400

    feat: Implement dynamic pricing and free selections in ModalOptionGroup
    - Updated the ModalOptionGroup component to display dynamic pricing based on the number of selections and free_option_limit for each option group.
    - Modified the logic to determine if an option should be free. Now, if the optionGroup has a free_option_limit that is not null and the number of selections is below the limit, the option will be free.
    - Addressed issues related to tracking selections and updating prices accurately.

--- component/ModalOptionGroup.jsx
+++ component/ModalOptionGroup.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/ModalOptionGroup.module.css";
 
-const ModalOptionGroup = ({ optionGroup, selectedOption, handleOptionChange, operationType }) => {
+const ModalOptionGroup = ({ optionGroup, handleOptionChange, selectionCounts, setSelectionCounts }) => {
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const nextIsFree = optionGroup.free_option_limit !== null && (selectionCounts[optionGroup.name] === undefined || selectionCounts[optionGroup.name] < optionGroup.free_option_limit);
+  const freeItemsCount = optionGroup.free_option_limit || 0;
+  const itemsWithZeroPrice = selectedOptions.slice(0, freeItemsCount);
+
+  useEffect(() => {
+    if (selectionCounts[optionGroup.name]) {
+      console.log(`Selection count: ${selectionCounts[optionGroup.name]}`);
+      console.log(optionGroup.free_option_limit, selectionCounts[optionGroup.name])
+      console.log(nextIsFree)
+    }
+  }, [selectionCounts, optionGroup]);
+
+  // Function to handle option selection
+  const handleOptionSelection = (option, group_name) => {
+    const isSelected = selectedOptions.some((selected) => selected.id === option.id);
+
+    if (isSelected) {
+      const updatedOptions = selectedOptions.filter((selected) => selected.id !== option.id);
+      setSelectedOptions(updatedOptions);
+
+      // Update the selection count for the respective group
+      setSelectionCounts((prevCounts) => ({
+        ...prevCounts,
+        [group_name]: prevCounts[group_name] - 1,
+      }));
+    } else {
+      const updatedOption = { ...option, group_name };
+      setSelectedOptions([...selectedOptions, updatedOption]);
+
+      // Update the selection count for the respective group
+      setSelectionCounts((prevCounts) => ({
+        ...prevCounts,
+        [group_name]: (prevCounts[group_name] || 0) + 1,
+      }));
+    }
+    handleOptionChange(optionGroup, option);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.optionDesc}>
@@ -10,37 +49,42 @@ const ModalOptionGroup = ({ optionGroup, selectedOption, handleOptionChange, ope
           {optionGroup.required ? "REQUIRED" : "OPTIONAL"}
         </div>
       </div>
-      <span className='comment'>
-      {optionGroup.free_option_limit === 1 ? "* first item is free"
-      : optionGroup.free_option_limit > 1 ? `* first ${optionGroup.free_option_limit} items are free`
-      : ""}
+      <span className="comment">
+        {optionGroup.free_option_limit === 1
+          ? "* first item is free"
+          : optionGroup.free_option_limit > 1
+          ? `* first ${optionGroup.free_option_limit} items are free`
+          : ""}
       </span>
 
       <div className={styles.optionChoices}>
-        {optionGroup.options.map((option, index) => (
-          <div className={styles.listOption} key={option.id}>
-            <label className={styles.label}>
-            {/* {console.log(selectedOption[option.id], option.id)} */}
-              <input
-                className={styles.option}
-                type={optionGroup.allow_multiple ? 'checkbox' : 'radio'}
-                value={option.name}
-                name={optionGroup.name}
-                {...(operationType === "edit" &&
-                selectedOption[option.id] &&
-                selectedOption[option.id].id === option.id && { checked: true })}
-                onChange={() => {
-                  handleOptionChange(optionGroup, option);
-                }}
-              />
-              <span>{option.name}</span>
-            </label>
-            <div>{option.additional_price === "00.00" ? "---" : `$${option.additional_price.toFixed(2)}`}</div>
-          </div>
-        ))}
+        {optionGroup.options.map((option, index) => {
+          const isSelected = selectedOptions.some((selected) => selected.id === option.id);
+          const shouldHaveZeroPrice = itemsWithZeroPrice.some((item) => item.id === option.id);
+          return (
+            <div className={styles.listOption} key={option.id}>
+              <label className={styles.label}>
+                <input
+                  className={styles.option}
+                  type={optionGroup.allow_multiple ? "checkbox" : "radio"}
+                  value={option.name}
+                  name={optionGroup.name}
+                  onChange={() => {
+                    handleOptionSelection(option, optionGroup.name);
+                  }}
+                  checked={isSelected}
+                />
+                <span>{option.name}</span>
+              </label>
+              <div>${shouldHaveZeroPrice ? "0.00" : nextIsFree ? "0.00" : option.additional_price.toFixed(2)}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 };
 
 export default ModalOptionGroup;
+
+
