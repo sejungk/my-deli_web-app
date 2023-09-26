33df6cf2e0629db5e5a5616ad22cacbf2c655cdf
Sejung <49921709+sejungk@users.noreply.github.com>
2023-09-25 23:18:04 -0400

    updated server var

--- component/MenuItemModal.jsx
+++ component/MenuItemModal.jsx
@@ -19,8 +19,9 @@ const MenuItemModal = ({itemId, id, closeModal, operationType, selectedOptions, 
   const scrollRef = useRef(null);
 
   useEffect(() => {
+    console.log(process.env.SERVER_URL)
     axios
-      .get(`${process.env.SERVER_URL}/api/menu-items/${id}`)
+      .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/menu-items/${id}`)
       .then((response) => {
         setMenuItemData(response.data);
         const initialQuantity = operationType === "edit" ? selectedQuantity : 1;
