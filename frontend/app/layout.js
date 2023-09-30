f23c3669e48b39fe3ca0f467889ac56640a01388
Sejung <49921709+sejungk@users.noreply.github.com>
2023-09-29 23:22:05 -0400

    feat: Add viewport meta tag to layout component
    - Included a viewport meta tag in the layout component's <head> section to control initial viewport dimensions and improve responsiveness.

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -9,6 +9,7 @@ export default function Layout({children}) {
     <html lang="en">
       <head>
         <title>My Deli</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
 
       <body className={inter.className}>
